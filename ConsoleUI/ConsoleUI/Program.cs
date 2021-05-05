6fb49456bcfa5ca8a73a25eb7ae0548faeee413d
Juba Koguashvili <64692459+juba97@users.noreply.github.com>
2021-05-05 22:22:46 +0400

    Write

---
+++ ConsoleUI/ConsoleUI/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Path();
+        }
+
+        static void Path()
+        {
+            string filepatch = @"D:\Test.txt";
+            //string[] lines = File.ReadAllLines(filepatch);
+            List<string> lines = new List<string>();
+            lines = File.ReadAllLines(filepatch).ToList();
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+
+            Console.Write("\nEnter a name to enter in the text document: ");
+            lines.Add(Console.ReadLine());
+            File.WriteAllLines(filepatch, lines);
+            
+        }
+    }
+}
